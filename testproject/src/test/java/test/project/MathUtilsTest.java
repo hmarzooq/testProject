be162a7aec43b4d2078d2ddf6ed71f9a39ac5259
Hasan Naqvi <engr.naqvi@gmail.com>
2021-04-13 16:38:00 +0800

    First commit. This is the base version of my project. All subsequent versions will be developed on this version.

---
+++ testproject/src/test/java/test/project/MathUtilsTest.java
@@ -0,0 +1,66 @@
+package test.project;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+	
+	private MathUtils mathUtils;
+	
+	
+	@BeforeEach
+	void initEach() {
+		mathUtils = new MathUtils();
+	}
+
+	@Nested
+	@DisplayName("when adding two numbers")
+	class AddTest {
+		@Test
+		void testAddingTwoPositives() {
+			assertEquals(2, mathUtils.add(1, 1), 
+					"Add method should return the sum of two numbers");
+		}
+		
+		@Test
+		void testAddingTwoNegatives() {
+			assertEquals(-2, mathUtils.add(-1, -1), 
+					"Add method should return the sum of two numbers");
+		}
+		
+		@Test
+		void testAddingAPositiveAndANegative() {
+			assertEquals(0, mathUtils.add(-1, 1), 
+					"Add method should return the sum of two numbers");
+		}
+	}
+	
+	@Test 
+	void testMultiply() {
+		assertAll(
+				() -> assertEquals(0, mathUtils.multiply(1, 0)),
+				() -> assertEquals(1, mathUtils.multiply(1, 1)),
+				() -> assertEquals(6, mathUtils.multiply(2, 3))
+				);
+	}
+	
+	@Test 
+	void computeCircleArea() {
+		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), 
+				"Should return right circle area");
+	}
+	
+	@Test
+	void testDivide() {
+		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), 
+				"Divide should throw ArithmeticException when denominator is zero");
+	}
+	
+	
+}
